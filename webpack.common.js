e869dd7064a47bc09591201c97593fe26bab7b75
hoanggaphan <phansihoang1998@gmail.com>
2021-02-05 19:35:38 +0700

    organize scss folder

--- webpack.common.js
+++ webpack.common.js
@@ -3,7 +3,9 @@ const HtmlWebpackPlugin = require('html-webpack-plugin');
 const path = require('path');
 
 module.exports = {
-  entry: {},
+  entry: {
+    index: ['./src/asset/scss/pages/index.scss'],
+  },
 
   output: {
     path: path.resolve(__dirname, 'dist'),
@@ -15,7 +17,7 @@ module.exports = {
     new HtmlWebpackPlugin({
       filename: 'index.html',
       template: './src/index.html',
-      chunks: [],
+      chunks: ['index'],
       inject: 'body',
       scriptLoading: 'blocking',
     }),
