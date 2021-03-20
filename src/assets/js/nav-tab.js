f879b07c36ccc417daa97fba796cbbdcd49f08f8
hoanggaphan <phansihoang1998@gmail.com>
2021-03-20 16:05:11 +0700

    completed lookbook page

--- src/assets/js/nav-tab.js
+++ src/assets/js/nav-tab.js
@@ -2,10 +2,16 @@ const tabs = document.querySelectorAll('[data-tab-target]');
 
 tabs.forEach((tab) => {
   tab.addEventListener('click', function () {
-    const tabActivated = document.querySelector('.is-tab-active');
-    tabActivated.classList.remove('is-tab-active');
+    const tabActivated = document.querySelector('[data-tab-target].is-tab-active');
+    if (tabActivated) {
+      tabActivated.classList.remove("is-tab-active");
+      tab.classList.add("is-tab-active");
+    }
+
+    const tabContentActivated = document.querySelector('[data-tab-content].is-tab-active');
+    tabContentActivated.classList.remove('is-tab-active');
     
-    const target = document.querySelector(tab.dataset.tabTarget);
-    target.classList.add('is-tab-active');
+    const tabContentTarget = document.querySelector(tab.dataset.tabTarget);
+    tabContentTarget.classList.add('is-tab-active');
   });
 });
